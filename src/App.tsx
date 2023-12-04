b05cc76110245894efbf058f57cc570ff46f7304
Ilya <lord-ilja@yandex.ru>
2023-12-04 20:40:13 +0300

    svg + path plugins Vite + add Container

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { Container } from 'components/Container'
+import { TheHeader } from 'components/TheHeader'
+
+import './App.css'
 function App() {
-  return
+  return (
+    <Container>
+      <TheHeader />
+    </Container>
+  )
 }
 
 export default App
