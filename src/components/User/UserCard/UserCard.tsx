971f764bbc5cb6cb5f67841530bb1f2b54e0e4eb
Ilya <lord-ilja@yandex.ru>
2023-12-15 22:07:41 +0300

    add UserTitle

--- src/components/User/UserCard/UserCard.tsx
+++ src/components/User/UserCard/UserCard.tsx
@@ -1,4 +1,5 @@
 import { UserStat } from 'components/User/UserStat'
+import { UserTitle } from 'components/User/UserTitle'
 
 import { LocalGithubUser } from 'interfaces/user.interface'
 import styles from './UserCard.module.scss'
@@ -8,6 +9,15 @@ interface UserCardProps extends LocalGithubUser {}
 export const UserCard = (props: UserCardProps) => {
   return (
     <div className={styles.userCard}>
+      <img src={props.avatar} alt={props.login} className={styles.avatar} />
+      <UserTitle
+        created={props.created}
+        login={props.login}
+        name={props.name}
+      />
+      <p className={`${styles.bio}${props.bio ? '' : ` ${styles.empty}`}`}>
+        {props.bio || 'This profile has no bio'}
+      </p>
       <UserStat
         repos={props.repos}
         followers={props.followers}
